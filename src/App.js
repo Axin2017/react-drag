950ef468dd1b6959f88924af2d457cc82a35f216
XinTan <838524739@qq.com>
2019-08-26 18:29:28 +0800

    按照例子测试拖拽

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
+import "antd/dist/antd.css";
+import { Layout } from 'antd';
+import ComponentsBox from './components/ComponentsBox'
+import { DragDropContext } from 'react-beautiful-dnd';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const { Header, Sider, Content } = Layout;
+class App extends React.Component {
+  render() {
+    return (
+      <DragDropContext
+        onDragStart={this.onDragStart}
+        onDragUpdate={this.onDragUpdate}
+        onDragEnd={this.onDragEnd}
+      >
+        <Layout className="layout">
+          <Header className="header">拖拽测试</Header>
+          <Layout className="main">
+            <Sider className="left-slider"><ComponentsBox /></Sider>
+            <Content>Content</Content>
+            {/* <Sider>Sider</Sider> */}
+          </Layout>
+        </Layout>
+
+      </DragDropContext>
+    )
+  };
 }
 
 export default App;
---
+++ src/components/ComponentsBox/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './ComponentsBox.css'
+import { Droppable } from 'react-beautiful-dnd';
+import { Draggable } from 'react-beautiful-dnd';
+
+class ComponentsBox extends React.Component {
+ 
+  render() {
+    return (
+        <div className="components-box">
+          <Draggable draggableId="draggable-1" index={0}>
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+              >
+                <h4>My draggable</h4>
+              </div>
+            )}
+          </Draggable>;
+        </div>
+    );
+  }
+}
+
+
+export default ComponentsBox
